5a89541f5904beced601f6dcd7376943c0be20a3
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-06-30 20:18:28 +0000

    Add errobox and errotype above form

--- src/app.js
+++ src/app.js
@@ -4,7 +4,6 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-import { format } from "prettier";
 
 window.onload = function() {
   //write your code here
@@ -24,6 +23,7 @@ form.addEventListener("submit", function(e) {
   let formatAmount = /[0-9]+/;
   let formatNames = /[A-Za-z]+/;
 
+  let errorBox = document.querySelector("#errorMsg");
   let card = document.querySelector("#cardNumber");
   let CVC = document.querySelector("#cardCVC");
   let amount = document.querySelector("#amount");
@@ -42,9 +42,9 @@ form.addEventListener("submit", function(e) {
   }
 
   if (empty.test(CVC.value)) {
-    errors.push(["cvc", "blank"]);
+    errors.push(["CVC", "blank"]);
   } else if (!formatCVC.test(CVC.value)) {
-    errors.push(["cvc", "typo"]);
+    errors.push(["CVC", "typo"]);
   }
 
   if (empty.test(amount.value)) {
@@ -71,10 +71,8 @@ form.addEventListener("submit", function(e) {
     errors.push(["city", "typo"]);
   }
 
-  if (empty.test(state.value)) {
+  if (state.value == "Pick a state") {
     errors.push(["state", "blank"]);
-  } else if (!formatNames.test(state.value)) {
-    errors.push(["state", "typo"]);
   }
 
   if (empty.test(postalCode.value)) {
@@ -87,5 +85,102 @@ form.addEventListener("submit", function(e) {
     errors.push(["message", "blank"]);
   }
 
-  console.log(errors);
+  /* Errors to show in form */
+  let typoErrors = 0;
+  let blankErrors = 0;
+  if (errors.length != 0) {
+    errors.forEach(element => {
+      if (element[0] == "card") {
+        card.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "CVC") {
+        CVC.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "amount") {
+        amount.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "firstName") {
+        firstName.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "lastName") {
+        lastName.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "city") {
+        city.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "state") {
+        state.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "postalCode") {
+        postalCode.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+
+      if (element[0] == "message") {
+        message.style.backgroundColor = "#ffcccb";
+        if (element[1] == "blank") {
+          blankErrors = 1;
+        } else if (element[1] == "typo") {
+          typoErrors = 1;
+        }
+      }
+    });
+  }
+
+  if (blankErrors == 1 && typoErrors == 0) {
+    errorBox.style.display = "block";
+  } else if (blankErrors == 1 && typoErrors == 1) {
+    errorBox.innerHTML = "Some fields have missing or invalid inputs";
+    errorBox.style.display = "block";
+  } else if (blankErrors == 0 && typoErrors == 1) {
+    errorBox.innerHTML = "Some fields have invalid inputs";
+    errorBox.style.display = "block";
+  }
+  console.log(blankErrors + " , " + typoErrors);
 });
